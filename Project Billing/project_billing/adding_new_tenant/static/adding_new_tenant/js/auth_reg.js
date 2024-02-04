3be033456a9aecfec66eb4639c332921f43fb918
Andrii Druzhha <xgodkeyx@gmail.com>
2024-02-04 23:28:32 +0200

    Кeworked сщву
    All code has been reworked. Corrected code structure. Added display of authorization and registration errors.

--- Project Billing/project_billing/adding_new_tenant/static/adding_new_tenant/js/auth_reg.js
+++ Project Billing/project_billing/adding_new_tenant/static/adding_new_tenant/js/auth_reg.js
@@ -1,104 +1,36 @@
-function register_html() {
-    $('.have_or_not_account').append(
-        `<p>Є акаунт?</p>
-        <button class="have_account" onclick="have_account()">Ввійти</button>`
-    )
-    $('.form_div').append(
-        `<p>Логін</p>
-        <input type="text" name="login" id="login">
-        <p>Ім'я</p>
-        <input type="text" name="first_name" id="first_name">
-        <p>Прізвище</p>
-        <input type="text" name="last_name" id="last_name">
-        <p>Пароль</p>
-        <input type="password" name="password" id="password">
-        <p>Пошта</p>
-        <input type="text" name="email" id="email">
-        <input type="hidden" name='register_id' id="register_id" value="1">
-        <button onclick="make_new_account()">Зареєструватись</button>`
-    )
-}
-
 $(document).ready(function() {
-    $.ajax({
-        url: '/adding_new_tenant/',
-        type: 'GET',
-        success: register_html()
+    $('button#theme').click(function(){
+        if ($('button#theme').hasClass('login')){
+            $('button#theme').removeClass('login').addClass('registr')
+            $('button#theme').html('Ввійти в акаунт')
+            $("input[name='csrfmiddlewaretoken'").after(
+                `<label for='first_name' id='first_name'>Ім'я</label>
+                <input type="text" name="first_name" id="first_name">
+                <label for='last_name' id="last_name">Прізвище</label>
+                <input type="text" name="last_name" id="last_name">`
+            )
+            $(`input[value='login'`).val('registr')
+        }
+        else if ($('button#theme').hasClass('registr')){
+            $('button#theme').removeClass('registr').addClass('login')
+            $('button#theme').html('Створити акаунт')
+            $(`input[value='registr'`).val('login')
+            $(`*#last_name`).remove()
+            $(`*#first_name`).remove()
+        }
+    })
+    $('button.submit').click(function(){
+        let form = $("form")
+        $.ajax({
+            url: "",
+            type: "POST",
+            data: form.serialize(),
+            success: function (response){
+                $(`.error`).empty()
+                $(`.error`).append(`
+                <h2>${response.error}</h2>
+                `)
+            }
+        })
     })
 })
-
-function html_login() {
-    $('.have_or_not_account').empty()
-    $('.have_or_not_account').append(
-        `<p>Немає акаунту?</p>
-        <button class="create_account" onclick='create_account()'>Створити</button>`
-    )
-    $('.form_div').empty()
-    $('.form_div').append(
-        `<p>Логін</p>
-        <input type="text" name="login_log" id='login_log'>
-        <p>Пароль</p>
-        <input type="password" name="password_log" id='password_log'>
-        <p>Пошта</p>
-        <input type="text" name="email_log" id='email_log'>
-        <input type="hidden" name='log_id' id="log_id" value="1">
-        <button onclick="log_into_account()">Ввійти в акаунт</button>`
-    )
-}
-
-function have_account(){
-    $.ajax({
-        url: '/adding_new_tenant/',
-        type: 'GET',
-        success: html_login()
-    })
-}
-
-
-
-function create_account(){
-    $.ajax({
-        url: '/adding_new_tenant/',
-        type: 'GET',
-        success: function(){
-            $('.have_or_not_account').empty()
-            $('.form_div').empty()
-            register_html()
-        }
-    })
-}
-
-function make_new_account() {
-    $.ajax({
-        url: '/adding_new_tenant/',
-        type: 'POST',
-        data: {
-            csrfmiddlewaretoken: $("input[name='csrfmiddlewaretoken']").val(),
-            login: $('#login').val(),
-            first_name: $('#first_name').val(),
-            last_name: $('#last_name').val(),
-            password: $('#password').val(),
-            email: $("#email").val(),
-            register_id: $('#register_id').val(),
-        },
-        success: html_login()
-    })
-}
-
-function log_into_account(){
-    $.ajax({
-        url: '/adding_new_tenant/',
-        type: 'POST',
-        data: {
-            csrfmiddlewaretoken: $("input[name='csrfmiddlewaretoken']").val(),
-            login: $('#login').val(),
-            password: $('#password').val(),
-            email: $("#email_log").val(),
-            log_id: $('#log_id').val(),
-        },
-        success: function(){
-            console.log(1)
-        }
-
-    })
-}
--- Project Billing/project_billing/adding_new_tenant/views.py
+++ Project Billing/project_billing/adding_new_tenant/views.py
@@ -9,28 +9,40 @@ from django.contrib.auth import login, authenticate, logout
 # Create your views here.
 def adding_new_tenant(request):
     context = {}
-    
     if request.method == 'POST':
-        if request.POST.get('register_id'):
-            log = request.POST.get('login')
+        setting = request.POST.get('setting')
+        log = request.POST.get('login')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        if setting == 'registr':
             first_name = request.POST.get('first_name')
             last_name = request.POST.get('last_name')
-            password = request.POST.get('password')
-            email = request.POST.get('email')
-            try:
-                User.objects.create(username=log, password=password, first_name=first_name, last_name=last_name, email=email,)
-            except IntegrityError:
-                context['error'] = 'Користувач з такими даними вже існує!'
-        if request.POST.get('log_id'):
-            log = request.POST.get('login_log')
-            password = request.POST.get('password_log')
-            email = request.POST.get('email_log')
-            user = User.objects.get(username=log, password=password, email=email)
-            login(request, user)
-            return redirect('main')
-                       
-                    
-    return render(request, 'adding_new_tenant/adding_new_tenant.html')
+            if log and first_name and last_name and password and email:
+                try:
+                    User.objects.create_user(username=log, password=password, first_name=first_name, last_name=last_name, email=email,)
+                except IntegrityError:
+                    context['error'] = 'Користувач з такими даними вже існує!'
+                finally:
+                    context['error'] = 'Акаунт зареєстровано!'
+            else:
+                context['error'] = "Не всі поля заповнено!"
+        if setting == 'login':
+            if log and password and email:
+                if authenticate(username = log, password = password):
+                    try:
+                        user = User.objects.get(username=log, password=password, email=email)
+                        login(request, user)
+                    except:
+                        context['error'] = 'Вибачте! Сталася невідома помилка...'
+                    finally:
+                        return redirect('main')
+                else:
+                    context['error'] = "Користувача не найдено!"
+            else:
+                context['error'] = "Не всі поля заповнено!"
+        return JsonResponse(context)
+    else:
+        return render(request, 'adding_new_tenant/adding_new_tenant.html')
 
 def main(request):
     context = {}
